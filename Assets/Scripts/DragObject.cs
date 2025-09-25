2147dd75d3e1770a4699b6397901f7a56b35d3fd
P3D\arthur.balazun <arthur.balazun@piktura.fr>
2025-09-25 12:21:50 +0200

    [GP] Drag System

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragObject : MonoBehaviour
 {
+    public LayerMask mask;
+
     GameObject _currentDragObject;
+    bool CheckLayer(int layer, LayerMask layerMask)
+    {
+        if (((1 << layer) & layerMask.value) != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (_currentDragObject != null)
+            if (_currentDragObject == null)
             {
                 RaycastHit hit = CastRay();
                 if (hit.collider != null)
                 {
+                    Debug.Log("IsDrag");
+                    if (CheckLayer(hit.collider.gameObject.layer, mask) == false)
+                    {
+                        return;
+                    }
+                    _currentDragObject = hit.collider.gameObject;
+                    Cursor.visible = false;
                 }
             }
             else
@@ -24,7 +44,9 @@ public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
         if (_currentDragObject != null)
         {
-
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(_currentDragObject.transform.position).z);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+            _currentDragObject.transform.position = new Vector2(worldPosition.x, worldPosition.y);
         }
     }
 
@@ -42,24 +64,8 @@ public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
         Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
         RaycastHit hit;
-        Physics.Raycast(worldMousePosNear, worldMousePosFar  - worldMousePosNear, out hit);
+        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
         return hit;
-    }
-
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("IsBeginDrag");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("IsDrag");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnEndDrag");
     }
 
 }
