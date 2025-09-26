30379bd5098640e19d433bb9e1b76ef86eb67318
ICondorI <clement.avez24@gmail.com>
2025-09-26 10:40:46 +0200

    Menu SoundSysteme

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Audio;
+public class SoundMixerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject MusicSource;
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float level)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level) * 20f);
+    }
+
+    public void SetSfxVolume(float level)
+    {
+        audioMixer.SetFloat("SfxVolume", Mathf.Log10(level) * 20f);
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(level) * 20f);
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(MusicSource);
+    }
+}
