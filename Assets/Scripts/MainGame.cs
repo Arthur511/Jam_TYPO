5405257f7dff7a4f189147835effd103364f3ac4
P3D\arthur.balazun <arthur.balazun@piktura.fr>
2025-09-26 09:38:34 +0200

    [GP] Advert Text

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,6 +1,6 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainGame : MonoBehaviour
@@ -8,9 +8,10 @@ public class MainGame : MonoBehaviour
     public static MainGame instance;
 
     public int ScoreToReach;
-    [HideInInspector]public int _placementToComplete;
+    [HideInInspector] public int _placementToComplete;
 
     [SerializeField] TMP_InputField _inputField;
+    [SerializeField] TextMeshProUGUI _textMeshPro;
     [SerializeField] List<SpawnableObject> _spawnableObjects;
     [SerializeField] Letters _lettersForLevel;
 
@@ -19,11 +20,29 @@ public class MainGame : MonoBehaviour
     HashSet<string> _alreadySpawnables = new HashSet<string>();
 
     bool _isInList = true;
+    bool _isTextVisible = false;
+
+    float _timer = 3f;
+    float _currentTimer;
 
     private void Awake()
     {
         instance = this;
     }
+
+    private void Update()
+    {
+        if (_isTextVisible)
+        {
+            _currentTimer -= Time.deltaTime;
+            if (_currentTimer <= 0)
+            {
+                DispearedText();
+                _isTextVisible = false;
+            }
+        }
+    }
+
 
     public void OnSubmit()
     {
@@ -34,6 +53,9 @@ public class MainGame : MonoBehaviour
             if (!_lettersForLevel._levelLetters.Contains(_lowerInput[i]))
             {
                 _isInList = false;
+                AppearedText("Utilisation d'une lettre inexistante dans ce niveau");
+                _currentTimer = _timer;
+                _isTextVisible = true;
                 break;
             }
         }
@@ -41,6 +63,9 @@ public class MainGame : MonoBehaviour
         if (_alreadySpawnables.Contains(_lowerInput))
         {
             _inputField.text = null;
+            AppearedText("Mot déjà utilisé !");
+            _currentTimer = _timer;
+            _isTextVisible = true;
             return;
         }
 
@@ -59,6 +84,17 @@ public class MainGame : MonoBehaviour
         _inputField.text = null;
     }
 
+
+    void AppearedText(string sentence)
+    {
+        _textMeshPro.text = sentence;
+        _textMeshPro.DOFade(1, 1f);
+    }
+
+    void DispearedText()
+    {
+        _textMeshPro.DOFade(0, 1f);
+    }
 
     public void WinRoom()
     {
